6b89997794d080fa987b487874f6caacad6ed97c
Dione Wilson <dionew1@gmail.com>
2020-10-16 16:57:30 -0600

    Complete CRUD for practice exercise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :books, only: [:index, :show]
+      resources :books
       # Without resources
       # get '/books', to: 'books#index'
       # get '/books/:id', to: 'books#show'
+      # post '/books', to: 'books#create'
+      # patch '/books/:id', to: 'books#update'
+      # put '/books/:id', to: 'books#update'
+      # delete '/books/:id', to: 'books#destroy'
     end
   end
 end
