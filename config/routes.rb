9e0d09716714784084531185fa41a2c51a14317d
Dione Wilson <dionew1@gmail.com>
2020-10-16 16:10:47 -0600

    Complete index and show actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :books, only: [:index]
+      resources :books, only: [:index, :show]
       # Without resources
       # get '/books', to: 'books#index'
+      # get '/books/:id', to: 'books#show'
     end
   end
 end
