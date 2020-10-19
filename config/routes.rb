14dfd1c3a844d1e69821e7b201a5aef4999955df
Dione Wilson <dionew1@gmail.com>
2020-10-19 10:18:39 -0600

    Add json response for store index and show

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :books
+      resources :stores, only: [:index, :show]
       # Without resources
       # get '/books', to: 'books#index'
       # get '/books/:id', to: 'books#show'
@@ -9,6 +10,8 @@ Rails.application.routes.draw do
       # patch '/books/:id', to: 'books#update'
       # put '/books/:id', to: 'books#update'
       # delete '/books/:id', to: 'books#destroy'
+      # get 'stores', to: 'stores#index'
+      # get 'stores/id', to: 'stores#show'
     end
   end
 end
